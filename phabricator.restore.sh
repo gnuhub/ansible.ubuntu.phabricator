d2b7971335aef3ba9a5ff114051487f9e2ac4c20
gnuhub <gnuhub@gmail.com>
2014-08-01 10:19:47 +0800

    编写数据恢复脚本 for #7

---
+++ phabricator.restore.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# 参数 $1 BACKUP_BUMBER
+
+# 恢复数据库
+cd /vagrant/backup/${1}
+gunzip -c backup.sql.gz | mysql
+# 恢复配置文件
+cd /vagrant/backup/${1}
+cp -rf local/ /var/www/html/phabricator-server/phabricator/conf/
